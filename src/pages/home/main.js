98052b2ba5d249794754f8ba874e904b9daa0f9c
BlameDeng <476961779@qq.com>
2018-11-08 13:23:16 +0800

    增加页面

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue/dist/vue.js'
+import '@/assets/global.scss'
+import './style.scss'
+
+const banners = [
+    'https://sunglassstore.oss-cn-hangzhou.aliyuncs.com/banners/banner1.jpg',
+    'https://sunglassstore.oss-cn-hangzhou.aliyuncs.com/banners/banner2.jpg',
+    'https://sunglassstore.oss-cn-hangzhou.aliyuncs.com/banners/banner3.jpg',
+    'https://sunglassstore.oss-cn-hangzhou.aliyuncs.com/banners/banner4.jpg'
+]
+banners.unshift(banners[0])
+banners.push(banners[banners.length - 1])
+
 new Vue({
     el: '#app',
     data: {
-        title: '首页'
+        banners
     }
 })
